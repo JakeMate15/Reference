2a2dedf6b1732c9899fb4c0d36f54e9017f0deb1
erik <erik.mofe@gmail.com>
2023-03-21 17:58:46 -0600

    Template

---
+++ Template/Template.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+#define endl '\n'
+#define IO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define YES cout<<"Yes\n";
+#define NO cout<<"No\n";
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define sortall(f) sort(all(f))
+#define forn(i,n) for(ll i=0;i<n;i++)
+
+const int MX = 0;
+
+using namespace std;
+
+typedef long long ll;
+
+void sol(){
+}
+
+int main(){
+    IO
+    int t=1;//cin>>t;
+
+    while(t--)
+        sol();
+}
