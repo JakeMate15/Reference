76ee5c876bd34c786390d33b4d7813d072155f0d
JakeMate15 <erik.mofe51@gmail.com>
2023-04-17 10:20:20 -0600

    busqueda binaria

--- BusquedaBinaria/BinarySearch.cpp
+++ BusquedaBinaria/BinarySearch.cpp
@@ -1,10 +1,21 @@
-\\Devuelve n o tam del vector si no encuentra un indice
-int find_lower(vector<int>& v, int x) {
-    int l = -1, r = v.size();
+\\Devuelve el maximo indice tal que v[i]<=x
+int closestToTheLeft(vector<int>& v, int x) {
+    int l = -1, r = v.size(),m;
     while (r > l + 1) {
-        int m = (l + r) / 2;
-        if (v[m] > x) r = m;
-        else l = m;
+        m = l+(r - l) / 2;
+        if (v[m] <= x) l = m;
+        else r = m;
     }
     return l;
+}
+
+\\Devuelve el indice del primer elemento v[i]>=x
+int closestToTheRight(vector<int>& v, int x){
+    int l=-1,r=n,m;
+    while(r>l+1){
+        m = l+(r-l)/2;
+        if(a[m]<x)  l=m;
+        else    r=m;
+    }
+    return r;
 }
